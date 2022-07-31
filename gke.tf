68bd217c1a41e6fde6900c8b818cb8727bfe96b7
unknown <damian.pawlow@gmail.com>
2022-07-31 22:34:20 +0200

    Standard GKE cluster in custom VPC network

--- gke.tf
+++ gke.tf
@@ -3,9 +3,7 @@ resource "google_container_cluster" "primary" {
   name     = var.gke_cluster
   location = var.gcp_region
   
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
+# Smallest possible node pool and immediately delete it to use separately managed Node Pool.
   remove_default_node_pool = true
   initial_node_count       = 1
 
